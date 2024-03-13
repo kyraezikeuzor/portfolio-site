42c776c322c2e02371601b2d6ea06d99ab71fd0f
Kyra Ezikeuzor <kyraezikeuzor@gmail.com>
2024-03-12 23:53:21 -0500

    Theme component fix attempt 2/?

--- components/Theme.tsx
+++ components/Theme.tsx
@@ -5,7 +5,7 @@ import Icon from './Icon'
 
 const ThemeButton = () => {
 
-    const [showDarkMode, setShowDarkMode] = useState(window.matchMedia('(prefers-color-scheme: dark)').matches)
+    const [showDarkMode, setShowDarkMode] = useState(false)
     const [systemMode, setSystemMode] = useState('')
     
 
